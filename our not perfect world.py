90455824e6fe29870e4303364002dbfedb2cbe00
DANIRU44 <98733158+DANIRU44@users.noreply.github.com>
2022-03-13 12:15:21 +0700

    Add files via upload

--- our not perfect world.py
+++ our not perfect world.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
+print("Powered by DANIRU44")
 print("здесь мы можете покидать монетку и убедьться, что школьная теория вероятностей плохо работает в реальном мире")
-attempt = int(input("сколько раз хотете совршить серию из 100 подкидываний?"))
+attempt = int(input("сколько раз хотете совeршить серию из 100 подкидываний?"))
 import random
 c=[]
 for r in range(attempt+1):
@@ -8,14 +9,14 @@ for r in range(attempt+1):
     for i in range (101):
         a =random.randint(0,1)
         if a==1:
-            b=b+1
+            b+=1
     c.append(b) 
 l = list(range(min(c),max(c)))
 r=[]
-h=min(c)#начинаем с минимального числа в списке
+h=min(c)    #начинаем с минимального числа в списке
 for o in range(len(l)): #прогоняем цикл столько раз, сколько элементов у нас в списке l (это нужно, чтобы график мог построиться)
-    r.append(c.count(h))#счтаем сколько у нас элементов находится в основном списке "с" и заносим кол-во в список r
-    h=+h#прибавляем к h 1 чтобы начать считать колл-во следующих элементов
+    r.append(c.count(h))    #счтаем сколько у нас элементов находится в основном списке "с" и заносим кол-во в список r
+    h+=1    #прибавляем к h 1 чтобы начать считать колл-во следующих элементов
 print("вот резульаты ваших серий")
 print(c)  
 print("минимально колличество одной выповшей стороны в серии: ",min(c))
@@ -24,9 +25,9 @@ print("в скольки сериях вероятность составила 
 print(l)
 print(r)
 plt.title("no perfect world")
-plt.xlabel("i don't know")
-plt.ylabel("numbers")
-plt.plot(r, l)
+plt.xlabel("numbers")
+plt.ylabel("number of results")
+plt.bar(l, r)
 plt.legend("legenda")
 plt.show()
 input()
